4c971310513c669351170a9d02e8c5ff8e060b92
chiragbadhe <badhechirag633@gmail.com>
2020-11-08 17:44:06 +0530

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,51 @@
 <template>
   <div
-    class="container-lg h-screen bg-gradient-to-r items-center from-purple-400 via-pink-500 to-red-500 justify-center flex flex-col items-center">
+    class="h-screen bg-gradient-to-r items-center from-purple-400 via-pink-500 to-red-500 justify-center flex flex-col items-center">
     <div class="">
       <div class="bg-white rounded-lg p-8 text-center flex flex-col items-center   " v-if="isSuccessScreenActive">
         <h1 class="font-bold text-6xl">Slam Book Created!</h1>
         <p class="text-4xl">Now , Share Link With Your Friends</p>
         <a class="text-3xl p-2 rounded-full pl-6 pr-6 bg-gray-200 m-4" id="link" :href="baseUrl + 'upload?name=' + name"
           style="color: blue">{{
-          baseUrl + 'upload?name=' + name
-        }}</a>
+          baseUrl + 'upload?name=' + name }}
+        </a>
 
         <input type="text" value="copyLink" id="userLink" v-model="link">
-
-        <button @click="copyLink()"
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full mt-4 ">
-          Copy Link
-        </button>
+        <button @click="copyLink()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded-full mt-4 ">Copy Link</button>
       </div>
-      <div class="bg-white text-center  border-gray-600 rounded-b-full overflow-auto"
-        v-else-if="isQuestionScreenActive">
-        <h1 class="text-4xl font-bold p-4 bg-gra-200 overflow-auto bg-gray-200 border-b-double border-8">Your Friends
-          Will Answer Thease 9 Questions About You</h1>
+
+
+      <div class="bg-white text-center  border-gray-600 rounded-b-full overflow-auto" v-else-if="isQuestionScreenActive">
+        <h1 class="text-4xl font-bold p-4 bg-gra-200 overflow-auto bg-gray-200 border-b-double border-8">Your Friends Will Answer Thease 9 Questions About You</h1>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">2. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">3. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">4. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <p class="font-bold text-xl">1. Any nickname for {{ name }}?</p>
+
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 m-4 rounded-full"
           @click="isSuccessScreenActive = true">FINISH</button>
       </div>
+
       <div class="bg-white p-8 rounded-lg text-center flex flex-col" v-else-if="isEnterNameScreenActive">
         <label class="block uppercase tracking-wide text-3xl pb-3text-xs font-bold mb-4" for="grid-password">
           Enter Your Name:
@@ -49,16 +59,16 @@
         </button>
       </div>
 
-      <div v-else class="text-center text-white m-6 flex flex-col items-center w-auto">
+      <div v-else class="h-full h-full text-center text-white m-6 flex flex-col items-center w-auto">
         <h1 class="text-6xl font-bold">What Your Friends Think About You?</h1>
         <p class="text-2xl p-6">
           Your Friends Will Give Answers Of Intersting Questions About You
         </p>
-        <img src="../images/main.png" alt="image" height="20%" width="20%">
+   
         <div class="text-center mt-10">
           <button class="border border-white-500 rounded p-3 bg-transparent text-white-500"
             @click="isEnterNameScreenActive = true">
-            Create Your Slambook
+            Create Your Slambook 
           </button>
         </div>
       </div>
--- pages/upload.vue
+++ pages/upload.vue
@@ -1,173 +1,254 @@
 <template>
+
+
   <div class="container-lg h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 justify-center flex flex-col items-center">
     <div class="font-bold bg-white rounded-lg overflow-auto" v-if="isSuccess">
-      <p class="text-4xl p-6 bg-gray-200">My Name :{{ userName }}</p>
-
-      <div class="text-xl pl-6 text-pink-500" v-for="question in questions" :key="question.index">
+      <p class="p-4 bg-gray-200 rounded-lg text-xl">Send Screenshot to :{{ senderName }}</p>
+      <p class="text-xl p-4 bg-gray-200">My Name :{{ userName }}</p>
+      <div class=" pl-6 text-pink-500" v-for="question in questions" :key="question.index">
         Question: {{ question.question }} <br />
-       <p class="text-xl text-indigo-700"> Answer: {{ question.answer }} </p>
+        <p class=" text-indigo-700"> Answer: {{ question.answer }} </p>
       </div>
     </div>
 
-      
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[8].show">
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[8].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question nine</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[8].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="isSuccess = true"> Finish </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[8].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="isSuccess = true"> Finish </a>
     </div>
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[7].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[7].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question eight</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[7].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(8)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(6)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[7].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(8)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(6)"> Previous </a>
     </div>
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[6].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[6].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question seven</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[6].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(7)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(5)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[6].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(7)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(5)"> Previous </a>
     </div>
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[5].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[5].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question six</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[5].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(6)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(4)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[5].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(6)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(4)"> Previous </a>
     </div>
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[4].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[4].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question five</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[4].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(6)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(3)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[4].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(6)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(3)"> Previous </a>
     </div>
 
-      <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[3].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[3].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question four</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[3].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(4)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(2)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[3].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(4)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(2)"> Previous </a>
     </div>
 
 
-     <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[2].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[2].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question three</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[2].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(3)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(1)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[2].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(3)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(1)"> Previous </a>
     </div>
 
-     <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[1].show">
+
+
+
+    <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[1].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question two</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[1].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(2)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(0)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[1].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(2)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(0)"> Previous </a>
     </div>
+
+
+
 
     <div class=" bg-white p-6 text-center m-5 flex flex-col rounded-lg" v-else-if="questions[0].show">
+        <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
       <h1 class="font-bold text-4xl p-4 pb-">Question One</h1>
-       <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="questions[0].answer" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(1)"> Next </a>
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#" @click="previous(0)"> Previous </a>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="questions[0].answer" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(1)"> Next </a>
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full" href="#"
+        @click="previous(0)"> Previous </a>
     </div>
 
+
+
+
     <div class="bg-white p-6 rounded-lg text-center flex flex-col" v-else>
-      <p class="font-bold text-4xl pb-5">Enter Name</p>
-      <input class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" v-model="userName" placeholder="Enter Name" />
-      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#" @click="show(0)"> Next </a>
+      <p class="bg-gray-200 font-bold pl-4 pr-4 rounded-lg text-2xl">Sent to you by :{{ senderName }}</p>
+      <p class="font-bold text-4xl p-5">Enter Your Name</p>
+      <input
+        class="bg-white rounded-lg border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+        v-model="userName" placeholder="Enter Name" />
+      <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded-full mb-2" href="#"
+        @click="show(0)"> Next </a>
     </div>
+
+
+
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      senderName: this.$route.query.name,
-      userName: '',
-      questions: [
-        {
-          index: 0,
-          question: 'Hello 0',
-          answer: '',
-          show: false,
-        },
+  export default {
+    data() {
+      return {
+        senderName: this.$route.query.name,
+        userName: '',
+        questions: [{
+            index: 0,
+            question: '1. Any nickname for' + this.senderName,
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 1,
-          question: 'Hello 1',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 1,
+            question: 'Hello 1',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 2,
-          question: 'Hello 2',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 2,
+            question: 'Hello 2',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 3,
-          question: 'Hello 3',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 3,
+            question: 'Hello 3',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 4,
-          question: 'Hello 4',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 4,
+            question: 'Hello 4',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 5,
-          question: 'Hello 5',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 5,
+            question: 'Hello 5',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 6,
-          question: 'Hello 6',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 6,
+            question: 'Hello 6',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 7,
-          question: 'Hello 7',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 7,
+            question: 'Hello 7',
+            answer: '',
+            show: false,
+          },
 
-        {
-          index: 8,
-          question: 'Hello 8',
-          answer: '',
-          show: false,
-        },
+          {
+            index: 8,
+            question: 'Hello 8',
+            answer: '',
+            show: false,
+          },
 
-        
-      ],
-      isSuccess: false,
-    }
-  },
 
-  methods: {
-    show(index) {
-        console.log(index)
-        console.log(this.questions[index+1])
-        this.questions[index].show = true
+        ],
+        isSuccess: false,
+      }
     },
 
-    previous(index){
+    methods: {
+      show(index) {
+        console.log(index)
+        console.log(this.questions[index + 1])
+        this.questions[index].show = true
+      },
+
+      previous(index) {
         this.questions[index + 1].show = false
-    }
-  },
-}
+      }
+    },
+  }
+
 </script>
